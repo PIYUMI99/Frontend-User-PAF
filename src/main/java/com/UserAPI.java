767347df2056761c9930d574c431c051e1bcdd66
Piyumika <piyumikaalmeda@gmail.com>
2022-05-15 23:57:37 +0530

    Finalize Project

--- src/main/java/com/UserAPI.java
+++ src/main/java/com/UserAPI.java
@@ -1,11 +1,19 @@
 package com;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.apache.tomcat.jni.User;
+
+
 
 /**
  * Servlet implementation class UserAPI
@@ -13,6 +21,31 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/UserAPI")
 public class UserAPI extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	User userObj = new User(); 
+	
+	
+	// Convert request parameters to a Map
+	private static Map getParasMap(HttpServletRequest request) 
+	{ 
+	 Map<String, String> map = new HashMap<String, String>(); 
+	try
+	 { 
+	 Scanner scanner = new Scanner(request.getInputStream(), "UTF-8"); 
+	 String queryString = scanner.hasNext() ? 
+	 scanner.useDelimiter("\\A").next() : ""; 
+	 scanner.close(); 
+	 String[] params = queryString.split("&"); 
+	 for (String param : params) 
+	 {
+		 String[] p = param.split("=");
+		 map.put(p[0], p[1]); 
+		 } 
+		 } 
+		catch (Exception e) 
+		 { 
+		 } 
+		return map; 
+		}
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -35,6 +68,18 @@ public class UserAPI extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		
+		
+		String output = userObj.insertUser(request.getParameter("Id"), 
+				   request.getParameter("Name"), 
+				   request.getParameter("Nic"), 
+				  request.getParameter("Address"),
+				   request.getParameter("TelNo")); 
+					request.getParameter("AccNo")); 
+		
+			response.getWriter().write(output);
+
+
 		doGet(request, response);
 	}
 
